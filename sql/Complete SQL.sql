d49ca23b1b2b4b4d2416c2982d00157a7614bc5c
gravitygo <chyle.andrei.lee@gmail.com>
2024-06-14 23:26:10 +0800

    added sql

--- sql/Complete SQL.sql
+++ sql/Complete SQL.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `itsecwb-mp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `itsecwb-mp`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: itsecwb-mp
@@ -14,6 +16,58 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `jwt_blacklist`
+--
+
+DROP TABLE IF EXISTS `jwt_blacklist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `jwt_blacklist` (
+  `jwt_token` varchar(500) NOT NULL,
+  `expiration_time` datetime NOT NULL,
+  PRIMARY KEY (`jwt_token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jwt_blacklist`
+--
+
+LOCK TABLES `jwt_blacklist` WRITE;
+/*!40000 ALTER TABLE `jwt_blacklist` DISABLE KEYS */;
+/*!40000 ALTER TABLE `jwt_blacklist` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` char(36) NOT NULL DEFAULT (uuid()),
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` char(60) NOT NULL,
+  `phoneNumber` varchar(11) NOT NULL,
+  `photoFileName` varchar(100) NOT NULL,
+  `isAdmin` tinyint(1) NOT NULL DEFAULT '0',
+  `refreshToken` char(60) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Dumping events for database 'itsecwb-mp'
@@ -55,4 +109,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-14  1:31:01
+-- Dump completed on 2024-06-14 23:24:42
