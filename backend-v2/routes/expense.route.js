bcd934f010d3ec7c0a1f1825b693d7202f40f6b8
gravitygo <chyle.andrei.lee@gmail.com>
2024-08-09 04:02:41 +0800

    add input validation in expense

--- backend-v2/routes/expense.route.js
+++ backend-v2/routes/expense.route.js
@@ -3,15 +3,16 @@ const { body } = require("express-validator");
 const router = express.Router();
 const expenseController = require("../controller/expense.controller");
 const { authenticateJWT } = require("../middleware/jwt");
+const { addExpenseValidation } = require("../middleware/expense");
 
 router.get("/get", authenticateJWT, expenseController.get);
 
-router.post("/add", authenticateJWT, expenseController.add);
+router.post("/add", authenticateJWT, addExpenseValidation, expenseController.add);
 
 router.delete("/:expenseID", authenticateJWT, expenseController.delete);
 
 router.get("/edit/:expenseID", authenticateJWT, expenseController.edit);
 
-router.post("/", authenticateJWT, expenseController.save);
+router.post("/", authenticateJWT, addExpenseValidation, expenseController.save);
 
 module.exports = router;
