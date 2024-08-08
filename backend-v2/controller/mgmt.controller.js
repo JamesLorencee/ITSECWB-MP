4f0c467054e4fdb8e5f36b9ef77a08faf2dc39cb
elliamaeang <elliamae_ang@dlsu.edu.ph>
2024-08-09 00:05:54 +0800

    Logger for Admin actions

--- backend-v2/controller/mgmt.controller.js
+++ backend-v2/controller/mgmt.controller.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const { logger } = require("../util/logger");
 
 exports.getUsers = async (req, res) => {
   try {
@@ -55,26 +56,36 @@ exports.getLogs = async (req, res) => {
 
 exports.setRole = async (req, res) => {
   const uid = req.params.userId;
+  const user = req.user.userId;
   try {
     const setRole = await User.setRole(uid);
     res.status(200).json({ ok: true, res: setRole });
+    logger.warn(`${uid} role changed by Admin ${user}.`);
   } catch (err) {
     let json = { ok: false, error: "Users Error" };
 
     if (process.env.DEBUG) json = { ...json, stack: err.stack };
+    logger.error(
+      `An attempt made by Admin ${user} to change the role of ${uid} failed.`
+    );
     return res.status(500).json(json);
   }
 };
 
 exports.setActive = async (req, res) => {
   const uid = req.params.userId;
+  const user = req.user.userId;
   try {
     const setActive = await User.setActive(uid);
     res.status(200).json({ ok: true, res: setActive });
+    logger.warn(`${uid} active status changed by Admin ${user}.`);
   } catch (err) {
     let json = { ok: false, error: "Users Error" };
 
     if (process.env.DEBUG) json = { ...json, stack: err.stack };
+    logger.error(
+      `An attempt made by Admin ${user} to change the active status of ${uid} failed.`
+    );
     return res.status(500).json(json);
   }
 };
