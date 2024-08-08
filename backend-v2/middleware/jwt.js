61e7e766f68884ad228ba9afed53544d19295487
elliamaeang <elliamae_ang@dlsu.edu.ph>
2024-08-08 22:39:56 +0800

    Stacktrace

--- backend-v2/middleware/jwt.js
+++ backend-v2/middleware/jwt.js
@@ -1,59 +1,66 @@
 require("dotenv").config();
 const { verify } = require("jsonwebtoken");
-const { clearJWTCookies, generateAccessToken, generateRefreshToken, setAccessCookies, setRefreshCookies } = require("../models/jwt");
+const {
+  clearJWTCookies,
+  generateAccessToken,
+  generateRefreshToken,
+  setAccessCookies,
+  setRefreshCookies,
+} = require("../models/jwt");
 const User = require("../models/user");
 
 exports.authenticateJWT = (req, res, next) => {
-    const access = req.cookies['aid']
-    const refresh = req.cookies['rid']
+  const access = req.cookies["aid"];
+  const refresh = req.cookies["rid"];
 
-    if (!access || !refresh) {
-        clearJWTCookies(res);
-        return res.status(401).json({ ok: false, message: "Invalid Authentication" });
-    }
+  if (!access || !refresh) {
+    clearJWTCookies(res);
+    return res.status(401).json({ ok: false, message: "Invalid Login" });
+  }
+  try {
+    const refreshPayload = verify(refresh, process.env.JWT_REFRESH_SECRET);
+
+    const payload = {
+      email: refreshPayload.email,
+      userId: refreshPayload.userId,
+      isAdmin: refreshPayload.isAdmin,
+    };
+
+    let accessPayload;
     try {
-        const refreshPayload = verify(refresh, process.env.JWT_REFRESH_SECRET);
-
-        const payload = {
-            email: refreshPayload.email,
-            userId: refreshPayload.userId,
-            isAdmin: refreshPayload.isAdmin,
-        };
-
-        let accessPayload;
-        try {
-            accessPayload = verify(access, process.env.JWT_ACCESS_SECRET);
-        } catch (err) {
-            if (err.name === 'TokenExpiredError') {
-                // Access token is expired, generate a new access token and refresh token
-                const newAccessToken = generateAccessToken(payload);
-                const newRefreshToken = generateRefreshToken(payload);
-
-                // Update cookies with new tokens
-                User.storeRefreshToken(newRefreshToken, payload.userId)
-                setAccessCookies(res, newAccessToken);
-                setRefreshCookies(res, newRefreshToken);
-
-                // Set the new tokens in the request for further processing
-                req.cookies['aid'] = newAccessToken;
-                req.cookies['rid'] = newRefreshToken;
-
-                // Re-verify the new access token
-                accessPayload = verify(newAccessToken, process.env.JWT_ACCESS_SECRET);
-
-            } else { throw new Error("Invalid Access Token"); }
-        }
-
-        if (accessPayload.userId != refreshPayload.userId) throw new Error("Invalid JWT");
-
-        req.user = accessPayload;
+      accessPayload = verify(access, process.env.JWT_ACCESS_SECRET);
     } catch (err) {
-        clearJWTCookies(res);
-        let json = { ok: false, error: "Invalid Authentication Please Log-in" };
+      if (err.name === "TokenExpiredError") {
+        // Access token is expired, generate a new access token and refresh token
+        const newAccessToken = generateAccessToken(payload);
+        const newRefreshToken = generateRefreshToken(payload);
 
-        if (process.env.DEBUG) json = { ...json, stack: err.stack };
+        // Update cookies with new tokens
+        User.storeRefreshToken(newRefreshToken, payload.userId);
+        setAccessCookies(res, newAccessToken);
+        setRefreshCookies(res, newRefreshToken);
 
-        return res.status(401).json(json);
+        // Set the new tokens in the request for further processing
+        req.cookies["aid"] = newAccessToken;
+        req.cookies["rid"] = newRefreshToken;
+
+        // Re-verify the new access token
+        accessPayload = verify(newAccessToken, process.env.JWT_ACCESS_SECRET);
+      } else {
+        throw new Error("Invalid Login");
+      }
     }
-    return next();
-}
+
+    if (accessPayload.userId != refreshPayload.userId)
+      throw new Error("Invalid Login");
+
+    req.user = accessPayload;
+  } catch (err) {
+    clearJWTCookies(res);
+    let json = { ok: false, error: "Invalid Login" };
+
+    if (process.env.DEBUG) json = { ...json, stack: err.stack };
+    return res.status(401).json(json);
+  }
+  return next();
+};
