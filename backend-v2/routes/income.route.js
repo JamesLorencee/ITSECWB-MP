0f6a289fb8e8c1dedc66a5e329cc555fc87d197d
czeska120 <franczeska_silvestre@dlsu.edu.ph>
2024-08-08 19:58:17 +0800

    added missing routes and controllers to v2

--- backend-v2/routes/income.route.js
+++ backend-v2/routes/income.route.js
@@ -1,32 +1,17 @@
 const express = require("express");
 const { body } = require("express-validator");
 const router = express.Router();
-const incomeController = require("../controllers/income.controller");
+const incomeController = require("../controller/income.controller");
 const { authenticateJWT } = require("../middleware/jwt");
 
-router.get("/get",
-    authenticateJWT,
-    incomeController.get
-);
+router.get("/get", authenticateJWT, incomeController.get);
 
-router.post("/add",
-    authenticateJWT,
-    incomeController.add
-);
+router.post("/add", authenticateJWT, incomeController.add);
 
-router.delete("/:incomeID",
-    authenticateJWT,
-    incomeController.delete
-);
+router.delete("/:incomeID", authenticateJWT, incomeController.delete);
 
-router.get("/edit/:incomeID",
-    authenticateJWT,
-    incomeController.edit
-);
+router.get("/edit/:incomeID", authenticateJWT, incomeController.edit);
 
-router.post("/",
-    authenticateJWT,
-    incomeController.save
-);
+router.post("/", authenticateJWT, incomeController.save);
 
 module.exports = router;
--- backend-v2/routes/mgmt.route.js
+++ backend-v2/routes/mgmt.route.js
@@ -2,43 +2,49 @@ const express = require("express");
 const { body } = require("express-validator");
 const router = express.Router();
 const { authenticateRoles } = require("../middleware/role");
-const mgmtController = require("../controllers/mgmt.controller");
+const mgmtController = require("../controller/mgmt.controller");
 const { authenticateJWT } = require("../middleware/jwt");
 
-router.get("/",
-    authenticateJWT,
-    authenticateRoles(true),
-    mgmtController.getUsers
+router.get(
+  "/",
+  authenticateJWT,
+  authenticateRoles(true),
+  mgmtController.getUsers
 );
 
-router.get("/filter/:role",
-    authenticateJWT,
-    authenticateRoles(true),
-    mgmtController.getUsersByRole
+router.get(
+  "/filter/:role",
+  authenticateJWT,
+  authenticateRoles(true),
+  mgmtController.getUsersByRole
 );
 
-router.get("/logs",
-    authenticateJWT,
-    authenticateRoles(true),
-    mgmtController.getLogs
+router.get(
+  "/logs",
+  authenticateJWT,
+  authenticateRoles(true),
+  mgmtController.getLogs
 );
 
-router.get("/edit/:userId",
-    authenticateJWT,
-    authenticateRoles(true),
-    mgmtController.getUserByID
+router.get(
+  "/edit/:userId",
+  authenticateJWT,
+  authenticateRoles(true),
+  mgmtController.getUserByID
 );
 
-router.post("/role/:userId",
-    authenticateJWT,
-    authenticateRoles(true),
-    mgmtController.setRole
+router.post(
+  "/role/:userId",
+  authenticateJWT,
+  authenticateRoles(true),
+  mgmtController.setRole
 );
 
-router.post("/active/:userId",
-    authenticateJWT,
-    authenticateRoles(true),
-    mgmtController.setActive
+router.post(
+  "/active/:userId",
+  authenticateJWT,
+  authenticateRoles(true),
+  mgmtController.setActive
 );
 
 module.exports = router;
