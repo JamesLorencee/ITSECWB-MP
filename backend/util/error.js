ca82887aeb472cb73cb7a7bf59117256bde40395
gravitygo <chyle.andrei.lee@gmail.com>
2024-08-09 21:00:04 +0800

    Remove Backend

--- backend/util/error.js
+++
@@ -1,9 +0,0 @@
-class HttpError extends Error {
-    constructor(status, httpMessage, error) {
-        super(httpMessage);
-        this.statusCode = status;
-        this.error = error;
-    }
-}
-
-module.exports = HttpError;
